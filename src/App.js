d3d40aa52d5ee681e7baba423ac1ab7bf393d47a
Yash Gupta <yashguptaaa333@gmail.com>
2022-11-18 14:55:53 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+
 function App() {
   return (
-  <>
-    <h1>App</h1>
-  </>
+    <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      <h1>App</h1>
+    </>
   );
 }
 
