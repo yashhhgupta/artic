c25a65e2aec62987ac1bae038dc911078611e58e
Yash Gupta <yashguptaaa333@gmail.com>
2022-11-18 14:45:50 +0530

    added gallery component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Gallery from "./component/Gallery";
+
 function App() {
   return (
   <>
     <h1>App</h1>
+    <Gallery></Gallery>
   </>
   );
 }
--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -1,97 +1,97 @@
 import React from 'react';
 import classes from './Gallery.module.css';
-const gallery=()=>{
+const Gallery=()=>{
     return(
         
-            <div class='gallery'>
+            <div className={classes.gallery}>
   <h1>CSS3 Animated Art Gallery</h1>
-  <div class='row'>    
-    <div class='thumbnail'>
-      <div class='photoContainer'>
+  <div className={classes.row}>    
+    <div className={classes.thumbnail}>
+      <div className={classes.photoContainer}>
         <a href='#'>
           <img src='https://images2.imgbox.com/88/24/cavsM98r_o.jpg'/>
-          <div class='photoInfo'>
+          <div className={classes.photoInfo}>
             <h3>"Loneliness of autumn"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
+            <span className={classes.paintingDate}>Leonid Afremov</span>
           </div>          
         </a>
       </div>
     </div>
-    <div class='thumbnail'>
-      <div class='photoContainer'>
+    <div className={classes.thumbnail}>
+      <div className={classes.photoContainer}>
         <a href='#'>
           <img src='https://images2.imgbox.com/13/10/fbt61vRw_o.jpg'/>
-          <div class='photoInfo'>
+          <div className={classes.photoInfo}>
             <h3>"Happiness"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
+            <span className={classes.paintingDate}>Leonid Afremov</span>
           </div>          
         </a>
       </div>
     </div>
-    <div class='thumbnail'>
-      <div class='photoContainer'>
+    <div className={classes.thumbnail}>
+      <div className={classes.photoContainer}>
         <a href='#'>
           <img src='https://images2.imgbox.com/70/de/5zEGOiaM_o.jpg'/>
-          <div class='photoInfo'>
+          <div className={classes.photoInfo}>
             <h3>"Last Dream"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
+            <span className={classes.paintingDate}>Leonid Afremov</span>
           </div>          
         </a>
       </div>
     </div>
-    <div class='thumbnail'>
-      <div class='photoContainer'>
+    <div className={classes.thumbnail}>
+      <div className={classes.photoContainer}>
         <a href='#'>
           <img src='https://images2.imgbox.com/9e/5b/h0yoZLbM_o.jpg'/>
-          <div class='photoInfo'>
+          <div className={classes.photoInfo}>
             <h3>"New Big Ben"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
+            <span className={classes.paintingDate}>Leonid Afremov</span>
           </div>          
         </a>
       </div>
     </div>
   </div>
-  <div class='row'>    
-    <div class='thumbnail'>
-      <div class='photoContainer'>
+  <div className={classes.row}>    
+    <div className={classes.thumbnail}>
+      <div className={classes.photoContainer}>
         <a href='#'>
           <img src='https://images2.imgbox.com/05/ac/sefBjSX3_o.jpg'></img>
-          <div class='photoInfo'>
+          <div className={classes.photoInfo}>
             <h3>"Autumn Foggy Day"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
+            <span className={classes.paintingDate}>Leonid Afremov</span>
           </div>          
         </a>
       </div>
     </div>
-    <div class='thumbnail'>
-      <div class='photoContainer'>
+    <div className={classes.thumbnail}>
+      <div className={classes.photoContainer}>
         <a href='#'>
           <img src='https://images2.imgbox.com/e3/43/PdIsPBrR_o.jpg'/>
-          <div class='photoInfo'>
+          <div className={classes.photoInfo}>
             <h3>"Paris, Eifel Tower"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
+            <span className={classes.paintingDate}>Leonid Afremov</span>
           </div>          
         </a>
       </div>
     </div>
-    <div class='thumbnail'>
-      <div class='photoContainer'>
+    <div className={classes.thumbnail}>
+      <div className={classes.photoContainer}>
         <a href='#'>
           <img src='https://images2.imgbox.com/05/04/zVWvnycP_o.jpg'/>
-          <div class='photoInfo'>
+          <div className={classes.photoInfo}>
             <h3>"He proposal"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
+            <span className={classes.paintingDate}>Leonid Afremov</span>
           </div>          
         </a>
       </div>
     </div>
-    <div class='thumbnail'>
-      <div class='photoContainer'>
+    <div className={classes.thumbnail}>
+      <div className={classes.photoContainer}>
         <a href='#'>
           <img src='https://images2.imgbox.com/f9/85/LRGslITj_o.jpg'/>
-          <div class='photoInfo'>
+          <div className={classes.photoInfo}>
             <h3>"The Bridge of Amsterdam"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
+            <span className={classes.paintingDate}>Leonid Afremov</span>
           </div>          
         </a>
       </div>
@@ -102,3 +102,4 @@ const gallery=()=>{
         </div>
     )
 }
+export default Gallery;
